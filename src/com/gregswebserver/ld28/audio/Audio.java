6939e36ff8a553dfdcbbb5146cf712eb47dd7cec
Daniel <dj.ackerson1@gmail.com>
2013-12-15 11:15:18 -0500

    added more wav files & added audio support

--- src/com/gregswebserver/ld28/audio/Audio.java
+++ src/com/gregswebserver/ld28/audio/Audio.java
@@ -1,41 +1,73 @@
 package com.gregswebserver.ld28.audio;
 
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Clip;
+import javax.sound.sampled.*;
+import java.io.IOException;
 
 public class Audio {
 
-    public static Audio beam = new Audio("/audio/beam.wav");
-    public static Audio step1 = new Audio("/audio/step_1.wav");
-    public static Audio step2 = new Audio("/audio/step_2.wav");
-    public static Audio bats = new Audio("/audio/bats.wav");
-    public static Audio cave_sound_1 = new Audio("/audio/cave_sound_1.wav");
-    public static Audio cave_sound_2 = new Audio("/audio/cave_sound_2.wav");
-    public static Audio cave_sound_3 = new Audio("/audio/cave_sound_3.wav");
-    public static Audio dripping = new Audio("/audio/dripping.wav");
+    public static Audio beam = new Audio("/audio/beam.wav");                    //end level
+    public static Audio step1 = new Audio("/audio/step_1.wav");                 //walking
+    public static Audio step2 = new Audio("/audio/step_2.wav");                 //walking
+    public static Audio bats = new Audio("/audio/bats.wav");                    //random ambiance
+    public static Audio cave_sound_1 = new Audio("/audio/cave_sound_1.wav");    //random ambiance
+    public static Audio cave_sound_2 = new Audio("/audio/cave_sound_2.wav");    //random ambiance
+    public static Audio cave_sound_3 = new Audio("/audio/cave_sound_3.wav");    //random ambiance
+    public static Audio dripping = new Audio("/audio/dripping.wav");            //background ambiance
+    public static Audio trumpet = new Audio("/audio/trumpet.wav");              //meet up accomplishment
+    public static Audio switch_player = new Audio("/audio/switch_player.wav");  //changing player focus
 
-    private static String path;
+    private AudioInputStream aInStream;
+    private Clip clip;
+    private FloatControl gainControl;
 
     public Audio(String path) {
-        this.path = path;
+        load(path);
     }
 
-    public static synchronized void playSound() {
-        new Thread(new Runnable() {
-            // The wrapper thread is unnecessary, unless it blocks on the
-            // Clip finishing; see comments.
-            public void run() {
-                try {
-                    Clip clip = AudioSystem.getClip();
-                    AudioInputStream inputStream = AudioSystem.getAudioInputStream(
-                            Audio.class.getResourceAsStream(path));
-                    clip.open(inputStream);
-                    clip.start();
-                } catch (Exception e) {
-                    System.err.println(e.getMessage());
-                }
-            }
-        }).start();
+    private void load(String path) {
+        try {
+            aInStream = AudioSystem.getAudioInputStream(Audio.class.getResourceAsStream(path));
+            clip = AudioSystem.getClip();
+            clip.open(aInStream);
+            gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+        } catch (UnsupportedAudioFileException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (LineUnavailableException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void play() {
+        clip.setMicrosecondPosition(0);
+        clip.start();
+    }
+
+    public void play(float dbAdjust) {
+        gainControl.setValue(dbAdjust); //Reduce/raise volume by (float) n decibels.
+        clip.setMicrosecondPosition(0); //i.e. dbAdjust = -10.0f will reduce by 10.0 decibels
+        clip.start();
+    }
+
+    public void loop() {
+        clip.setMicrosecondPosition(0);
+        clip.loop(Clip.LOOP_CONTINUOUSLY);
+        clip.start();
+    }
+
+    public void loop(float dbAdjust) {
+        gainControl.setValue(dbAdjust);
+        clip.setMicrosecondPosition(0);
+        clip.loop(Clip.LOOP_CONTINUOUSLY);
+        clip.start();
+    }
+
+    public void stop() {
+        clip.stop();
+    }
+
+    public Clip getClip() {
+        return clip;
     }
 }
