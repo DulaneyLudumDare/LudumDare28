58e18ef371c60f0ca54ec7a6f19e02023a2eb4a2
Daniel <dj.ackerson1@gmail.com>
2013-12-15 00:25:11 -0500

    added wav files to res

--- src/com/gregswebserver/ld28/audio/Audio.java
+++ src/com/gregswebserver/ld28/audio/Audio.java
@@ -7,8 +7,13 @@ import javax.sound.sampled.Clip;
 public class Audio {
 
     public static Audio beam = new Audio("/audio/beam.wav");
-    public static Audio step1 = new Audio("/audio/step1.wav");
-    public static Audio step2 = new Audio("/audio/step2.wav");
+    public static Audio step1 = new Audio("/audio/step_1.wav");
+    public static Audio step2 = new Audio("/audio/step_2.wav");
+    public static Audio bats = new Audio("/audio/bats.wav");
+    public static Audio cave_sound_1 = new Audio("/audio/cave_sound_1.wav");
+    public static Audio cave_sound_2 = new Audio("/audio/cave_sound_2.wav");
+    public static Audio cave_sound_3 = new Audio("/audio/cave_sound_3.wav");
+    public static Audio dripping = new Audio("/audio/dripping.wav");
 
     private static String path;
 
